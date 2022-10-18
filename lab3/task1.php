69001592f97c6ea7367c4d2f0286ecf2c90754aa
marharita08 <72861258+marharita08@users.noreply.github.com>
2022-10-18 10:48:39 +0300

    Create task1.php

---
+++ lab3/task1.php
@@ -0,0 +1,17 @@
+<?php 
+class A { 
+	function example() { 
+		echo "This is parent function A::example().<br>"; 
+	}
+} 
+
+class B extends A { 
+	function example() { 
+		echo "This is overriden function B::example().<br>"; 
+		A::example(); 
+	}
+} 
+
+$b = new B; 
+$b->example();
+?>
