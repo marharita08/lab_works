5cc0bc7fea48ff16a6955fafbb410d4b5f8b962c
marharita08 <72861258+marharita08@users.noreply.github.com>
2022-10-21 18:17:08 +0300

    Create task1.php

---
+++ lab4/task1.php
@@ -0,0 +1,17 @@
+<?php 
+class Coor { 
+	private $text;
+	static $count = 0;
+	function __construct($text) { 
+		$this->text=$text;
+		Coor::$count++;
+	} 
+	function getName() { 
+		echo "<p>Name: $this->text <br>"; 
+	}
+} 
+echo Coor::$count."<br>";
+$object = new Coor("Nick"); 
+echo Coor::$count."<br>";
+$object->Getname(); 
+?>
